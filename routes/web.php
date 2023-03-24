a4a6b0d637207e451ec51f28f93662466ff2a0a5
unknown <indahrani@student.telkomuniversity.ac.id>
2023-03-24 22:15:01 +0700

    end

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Models\Cart;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/product', ProductController::class);
+Route::resource('/katalog', KatalogController::class);
+Route::get('/cart', []);
+
+Route::get('/cart', function () {
+    return view('cart.index', [
+        'carts' => Cart::all(),
+    ]);
+});
